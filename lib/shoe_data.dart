08d98af1a822cd347e8ce5e8db01dc45a2fd66ad
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-05-17 18:02:11 +0300

    created the data model and datas of shoes

---
+++ lib/shoe_data.dart
@@ -0,0 +1,89 @@
+import 'shoe_model.dart';
+
+Shoe shoe1 = Shoe(
+  name: 'nahoo',
+  price: 219.99,
+  mainImage: 'asset/images/qc_brown_brown.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_brown_brown.webp',
+    'asset/images/qc_brown_brown_pair.webp',
+  ],
+);
+Shoe shoe2 = Shoe(
+  name: 'Ebs ebs',
+  price: 259.99,
+  mainImage: 'asset/images/qc_black_sideview.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_black_sideview.webp',
+    'asset/images/qc_black_two_pair_topview.webp',
+  ],
+);
+Shoe shoe3 = Shoe(
+  name: 'Abay abay',
+  price: 149.99,
+  mainImage: 'asset/images/qc_white.png',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_white.png',
+    'asset/images/qc_white_pair_topview.webp',
+    'asset/images/qc_white_backview.webp',
+  ],
+);
+Shoe shoe4 = Shoe(
+  name: 'Arts arts',
+  price: 399.99,
+  mainImage: 'asset/images/qc_white_shiro.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_white_shiro.webp',
+    'asset/images/qc_white_shiro_pair.webp',
+  ],
+);
+Shoe shoe5 = Shoe(
+  name: 'Arts arts',
+  price: 399.99,
+  mainImage: 'asset/images/qc_white_shiro.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_white_shiro.webp',
+    'asset/images/qc_white_shiro_pair.webp',
+  ],
+);
+Shoe shoe6 = Shoe(
+  name: 'Arts arts',
+  price: 399.99,
+  mainImage: 'asset/images/qc_white_shiro.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_white_shiro.webp',
+    'asset/images/qc_white_shiro_pair.webp',
+  ],
+);
+Shoe shoe7 = Shoe(
+  name: 'Arts arts',
+  price: 399.99,
+  mainImage: 'asset/images/qc_white_shiro.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_white_shiro.webp',
+    'asset/images/qc_white_shiro_pair.webp',
+  ],
+);
+List<Shoe> allTheShoes = [
+  shoe1,
+  shoe2,
+  shoe3,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+];
---
+++ lib/shoe_model.dart
@@ -0,0 +1,15 @@
+class Shoe {
+  String name;
+  double price;
+  String mainImage;
+  String description;
+  List<String> images;
+
+  Shoe({
+    required this.name,
+    required this.price,
+    required this.mainImage,
+    required this.description,
+    required this.images,
+  });
+}
