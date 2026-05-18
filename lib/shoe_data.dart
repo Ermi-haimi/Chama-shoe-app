b118ba86f41199dec5fafd0dc94ecb77b1f1c2e7
Ermias Haimanot <ermiashtsegaye@gmail.com>
2026-05-18 11:34:00 +0300

    updated the data with more images

--- lib/shoe_data.dart
+++ lib/shoe_data.dart
@@ -1,28 +1,18 @@
 import 'shoe_model.dart';
 
 Shoe shoe1 = Shoe(
-  name: 'nahoo',
-  price: 219.99,
-  mainImage: 'asset/images/qc_brown_brown.webp',
+  name: 'Arts arts',
+  price: 249.99,
+  mainImage: 'asset/images/qc_pink_and_brown_tennis_pair.webp',
   description:
       'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
   images: [
-    'asset/images/qc_brown_brown.webp',
-    'asset/images/qc_brown_brown_pair.webp',
+    'asset/images/qc_pink_and_brown_tennis_pair.webp',
+    'asset/images/qc_pink_and_brown_tennis.webp',
+    'asset/images/qc_brown_tennis.webp',
   ],
 );
 Shoe shoe2 = Shoe(
-  name: 'Ebs ebs',
-  price: 259.99,
-  mainImage: 'asset/images/qc_black_sideview.webp',
-  description:
-      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
-  images: [
-    'asset/images/qc_black_sideview.webp',
-    'asset/images/qc_black_two_pair_topview.webp',
-  ],
-);
-Shoe shoe3 = Shoe(
   name: 'Abay abay',
   price: 149.99,
   mainImage: 'asset/images/qc_white.png',
@@ -31,53 +21,80 @@ Shoe shoe3 = Shoe(
   images: [
     'asset/images/qc_white.png',
     'asset/images/qc_white_pair_topview.webp',
-    'asset/images/qc_white_backview.webp',
+    'asset/images/qc_lightblue_white_pair.webp',
+  ],
+);
+Shoe shoe3 = Shoe(
+  name: 'Arts arts',
+  price: 399.99,
+  mainImage: 'asset/images/qc_blue_light_blue_pair.png',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_blue_light_blue_pair.png',
+    'asset/images/qc_blue_light_blue.png',
+    'asset/images/qc_blue.webp',
   ],
 );
 Shoe shoe4 = Shoe(
   name: 'Arts arts',
   price: 399.99,
-  mainImage: 'asset/images/qc_white_shiro.webp',
+  mainImage: 'asset/images/qc_white_shiro_pair.webp',
   description:
       'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
   images: [
-    'asset/images/qc_white_shiro.webp',
     'asset/images/qc_white_shiro_pair.webp',
+    'asset/images/qc_white_shiro.webp',
   ],
 );
 Shoe shoe5 = Shoe(
-  name: 'Arts arts',
-  price: 399.99,
-  mainImage: 'asset/images/qc_white_shiro.webp',
+  name: 'nahoo',
+  price: 219.99,
+  mainImage: 'asset/images/qc_brown_brown_pair.webp',
   description:
       'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
   images: [
-    'asset/images/qc_white_shiro.webp',
-    'asset/images/qc_white_shiro_pair.webp',
+    'asset/images/qc_brown_brown_pair.webp',
+    'asset/images/qc_brown_brown.png',
   ],
 );
 Shoe shoe6 = Shoe(
   name: 'Arts arts',
-  price: 399.99,
-  mainImage: 'asset/images/qc_white_shiro.webp',
+  price: 199.99,
+  mainImage: 'asset/images/qc_lightblue_pair.webp',
   description:
       'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
   images: [
-    'asset/images/qc_white_shiro.webp',
-    'asset/images/qc_white_shiro_pair.webp',
+    'asset/images/qc_lightblue_pair.webp',
+    'asset/images/qc_lightblue_pair_backview.webp',
+    'asset/images/qc_blue_.webp',
   ],
 );
 Shoe shoe7 = Shoe(
-  name: 'Arts arts',
-  price: 399.99,
-  mainImage: 'asset/images/qc_white_shiro.webp',
+  name: 'Ebs ebs',
+  price: 259.99,
+  mainImage: 'asset/images/qc_pink_brown.png',
   description:
       'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
   images: [
-    'asset/images/qc_white_shiro.webp',
-    'asset/images/qc_white_shiro_pair.webp',
+    'asset/images/qc_pink_brown.png',
+    'asset/images/qc_brown.webp',
+    'asset/images/qc_gg.webp',
   ],
 );
+Shoe shoe8 = Shoe(
+  name: 'Ebs ebs',
+  price: 259.99,
+  mainImage: 'asset/images/qc_black_sideview.webp',
+  description:
+      'beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful beautiful',
+  images: [
+    'asset/images/qc_black_sideview.webp',
+    // 'asset/images/qc_black_two_pair_topview.webp',
+    'asset/images/qc_black_black_sideview.webp',
+  ],
+);
+
 List<Shoe> allTheShoes = [
   shoe1,
   shoe2,
@@ -86,4 +103,14 @@ List<Shoe> allTheShoes = [
   shoe5,
   shoe6,
   shoe7,
+  shoe8,
+];
+
+List<String> shoeSize = [
+  '5',
+  '5.5',
+  '6',
+  '6.6',
+  '7',
+  '7.5',
 ];
